562eb0e5777501de11a7f4a5734f5552e681d1bd
Jehp00 <Jehp01104006@gmail.com>
2022-09-06 17:01:34 -0500

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,19 +10,29 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i;
 	size_t j;
-	int tmp;
+	size_t tmp;
+	size_t flag;
+
+	if (array == NULL || size == 0)
+	{
+		return;
+	}
 
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < (size - i - 1); j++)
+		flag = 0;
+		for (j = 0; j < size - i - 1; j++)
 		{
-			if (array[j] > array[i] + 1)
+			if (array[j] > array[j + 1])
 			{
 				tmp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = tmp;
+				flag = 1;
 				print_array(array, size);
 			}
 		}
+		if (flag == 0)
+			break;
 	}
 }
