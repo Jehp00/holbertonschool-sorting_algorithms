fcc68e613e7bb839921f5f29747b1edfae3935da
Jehp00 <Jehp01104006@gmail.com>
2022-09-07 10:47:40 -0500

    selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,46 @@
+#include "sort.h"
+
+/**
+ * swap - it swaps two numbers.
+ * @a: first integer.
+ * @b: second integer.
+ **/
+
+void swap(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * selection_sort - sorts an array using
+ * selection sort algorithm.
+ * @array: a given array.
+ * @size: size of the array.
+ **/
+
+void selection_sort(int *array, size_t size)
+{
+	unsigned int i, j, pos_min_val;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		pos_min_val = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[pos_min_val] > array[j])
+				pos_min_val = j;
+		}
+		if (pos_min_val != i)
+		{
+			swap(&array[i], &array[pos_min_val]);
+			print_array(array, size);
+		}
+	}
+}
--- sort.h
+++ sort.h
@@ -21,9 +21,11 @@ typedef struct listint_s
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
-void Swap_nodes_themselves(listint_t **list, listint_t **node1, listint_t **node2);
+void Swap_nodes_themselves(listint_t **list,
+listint_t **node1, listint_t **node2);
 listint_t *create_listint(const int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+void swap(int *a, int *b);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
